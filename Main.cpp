3779c728b17922f1bf89d054ae05206cfa39ba3b
iwatake2222 <take.iwiw2222@gmail.com>
2021-06-20 08:56:19 +0900

    Add copyright notice, Update document

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,18 @@
+/* Copyright 2021 iwatake2222
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
 /*** Include ***/
 /* for general */
 #include <cstdint>
@@ -18,9 +33,7 @@
 #include "Uart.h"
 
 /*** Macro ***/
-#define IMAGE_NAME   RESOURCE_DIR"/body_female.jpg"
 #define WORK_DIR     RESOURCE_DIR
-#define LOOP_NUM_FOR_TIME_MEASUREMENT 0
 
 int32_t main()
 {
@@ -38,39 +51,33 @@ int32_t main()
 	ImageProcessor_initialize(&inputParam);
 
 	/* Initialize camera */
-	int32_t originalImageWidth = 640;
-	int32_t originalImageHeight = 480;
-
 	static cv::VideoCapture cap;
 #ifdef _WIN32
 	cap = cv::VideoCapture(cv::CAP_DSHOW + 0);
 #else
 	cap = cv::VideoCapture(0);
 #endif
-	cap.set(cv::CAP_PROP_FRAME_WIDTH, originalImageWidth);
-	cap.set(cv::CAP_PROP_FRAME_HEIGHT, originalImageHeight);
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
 	// cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('B', 'G', 'R', '3'));
 	cap.set(cv::CAP_PROP_BUFFERSIZE, 1);
 
 	char command[32] = "";
 
 	while (1) {
-		const auto& timeAll0 = std::chrono::steady_clock::now();
-		/*** Read image ***/
-		const auto& timeCap0 = std::chrono::steady_clock::now();
+		/* Read image */
 		cv::Mat originalImage;
 		cap.read(originalImage);
-		const auto& timeCap1 = std::chrono::steady_clock::now();
 
 		/* Call image processor library */
-		const auto& timeProcess0 = std::chrono::steady_clock::now();
 		OUTPUT_PARAM outputParam;
 		ImageProcessor_process(&originalImage, &outputParam);
-		const auto& timeProcess1 = std::chrono::steady_clock::now();
 
+		/* Display the processed image */
 		cv::imshow("test", originalImage);
 		if (cv::waitKey(1) == 'q') break;
 
+		/* Send command when it's updated */
 		if (outputParam.command[0] != 0 && strncmp(command, outputParam.command, sizeof(command)) != 0) {
 			strncpy(command, outputParam.command, sizeof(command));
 			printf("CMD = %s\n", command);
@@ -78,21 +85,6 @@ int32_t main()
 				printf("[ERR] uart.send\n");
 			}
 		}
-
-		// char buffer[100];
-		// int32_t recvLen = uart.recv(buffer, sizeof(buffer));
-		// if (recvLen < 0) {
-		// 	printf("[ERR] uart.recv\n");
-		// } else {
-		// 	buffer[recvLen] = '\0';
-		// 	printf("%s", buffer);
-		// }
-
-		// const auto& timeAll1 = std::chrono::steady_clock::now();
-		// printf("Total time = %.3lf [msec]\n", (timeAll1 - timeAll0).count() / 1000000.0);
-		// printf("Capture time = %.3lf [msec]\n", (timeCap1 - timeCap0).count() / 1000000.0);
-		// printf("Image processing time = %.3lf [msec]\n", (timeProcess1 - timeProcess0).count() / 1000000.0);
-		// printf("========\n");
 	}
 
 	/* Fianlize image processor library */
--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 # Petoi Bittle Controlled by Pose
+- A project to control Petoi Bittle using human pose
+- Human pose is estimated using MoveNet and TensorFlow Lite
+
+![00_doc/pic.jpg](00_doc/pic.jpg)
+
+## YouTube
+[![00_doc/youtube.jpg](00_doc/youtube.jpg)](https://www.youtube.com/watch?v=ZRhm6DueARo)
+
+
+## System overview
+![00_doc/system.png](00_doc/system.png)
+
+
+## Components
+- Petoi Bittle
+    - https://www.petoi.com/
+    - How to assemble: https://bittle.petoi.com/
+    - Solder a 2x5 socket on NyBoards
+- Raspberry Pi 4
+    - 2021-05-07-raspios-buster-arm64
+    - enable VNC, CAMERA, UART (but disable UART console)
+    - Install OpenCV (self build needed)
+- Raspberry Pi Camera v2.1
+
+## How to Run
+- Connect the Raspberry Pi 4 and Petoi Bittle
+- Build the project and run it
+
+```
+git clone https://github.com/iwatake2222/bittle_controlled_by_pose.git
+cd bittle_controlled_by_pose
+git submodule update --init --recursive
+cd InferenceHelper/ThirdParty/tensorflow
+chmod +x tensorflow/lite/tools/make/download_dependencies.sh
+tensorflow/lite/tools/make/download_dependencies.sh
+
+mkdir build && cd build
+cmake ..
+make
+./main
+```
+
+## Warning
+- It gets super hot !!
 
 # License
 - bittle_controlled_by_pose
